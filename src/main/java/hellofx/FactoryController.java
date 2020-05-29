11c660f6da820100f0fc3828bacc149bfac1e975
Amaury Daniel Palao Garcia <pala2402@usherbrooke.ca>
2020-05-29 13:31:20 -0400

    les id sont fait, sauf pour toolBar

--- src/main/java/hellofx/FactoryController.java
+++ src/main/java/hellofx/FactoryController.java
@@ -1,19 +1,53 @@
 package hellofx;
   
-import javafx.fxml.FXML; 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TitledPane;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FactoryController {
-
+	
+	//code de base --pas touche--
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
+    //--pas touche--
 
+    //variable pour le MenuBar
+    @FXML
+    private Menu menuFile;
+    @FXML
+    private Menu menuEdit;
+    @FXML
+    private Menu menuMore;
+    
+    //label du bas de page
+    @FXML
+    private Label labelMode;
+    
+    //Menu des tiltedPane sur la gauche de la fenetre
+    @FXML
+    private TitledPane tiltedPaneEnergy;
+    @FXML
+    private TitledPane titledPaneInversion;
+    @FXML
+    private TitledPane titledPaneStrategy;
+    @FXML
+    private TitledPane titledPaneEstimator;
+    
+    //change de travail principal
+    @FXML
+    private ScrollPane tableauTravail;
+    
     @FXML
     void initialize() {
 
     }
+    
 }
